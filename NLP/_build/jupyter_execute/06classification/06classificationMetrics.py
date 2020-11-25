114e701f77b5cec5cea1953ed06659ef7c6dcb19
Johannes Maucher <johannes@Johanness-iMac.fritz.box>
2020-11-25 17:33:56 +0100

    mc

---
+++ NLP/_build/jupyter_execute/06classification/06classificationMetrics.py
@@ -0,0 +1,121 @@
+# Validation of Classifiers
+
+<img src="http://maucher.home.hdm-stuttgart.de/Pics/SupervisedLearningSchemaValidation.png" style="width:700px" align="center">
+
+Text classification is part of supervised Machine Learning (ML). As depicted in the picture above, supervised ML relies on labeled data - not only for training, but also for model testing. It is important that training- and test-datasets are disjoint.
+
+Once a model is trained, it is applied on test-data and calculates predictions for the test-data input. Since for test-data also the true label (output) is known, these true labels can be compared with the predicted labels. Based on this comparison, different  **metrics** for classifier evaluations can be calculated. The most important classifier metrics are described below.
+
+Assume that for 10 test-data samples the true and predicted labels (class-indeces) are as listed in the table below:
+
+| True Label | Predicted Label |
+|:----------:|:---------------:|
+|      0     |        0        |
+|      0     |        1        |
+|      1     |        1        |
+|      1     |        1        |
+|      1     |        0        |
+|      0     |        0        |
+|      1     |        0        |
+|      0     |        0        |
+|      0     |        0        |
+|      0     |        0        |
+
+All of the metrics described below, can be calculated from this comparison. 
+
+## Confusion matrix
+
+The confusion matrix contains for each pair of classes $i$ and $j$, the number of class $i$ elements, which have been predicted to be class $j$. Usually, each row corresponds to a true-class label and each column corresponds to a predicted class label. 
+
+In the general 2-class confusion matrix, depicted below, the class labels are $P$ (positive) and $N$. The matrix entries are then
+
+* **TP (True Positives):** Number of samples, which belong to class $P$
+ and have correctly been predicted to be class $P$
+ 
+* **TN (True Negative):** Number of samples, which belong to class $N$
+ and have correctly been predicted to be class $N$
+ 
+* **FP (False Positives):** Number of samples, which belong to class $N$
+ but have falsely been predicted to be class $P$
+ 
+* **FN (False Negatives):** Number of samples, which belong to class $P$
+ but have falsely been predicted to be class $N$
+ 
+<img src="http://maucher.home.hdm-stuttgart.de/Pics/confusionMatrixTN.png" style="width:200px" align="center">
+
+For the given example of predicted and true class labels the confusion matrix is:
+
+<img src="http://maucher.home.hdm-stuttgart.de/Pics/confusionMatrixExample.png" style="width:200px" align="center">
+
+
+## Accuracy
+
+Accuracy is the ratio of correct predictions among all predictions. For the 2-class problem and the labels $P$ and $N$, accuracy can be calculated from the entries of the confusion matrix:
+
+$$
+Acc =\frac{TP+TN}{TP+TN+FP+FN}
+$$
+
+
+In the example the accuracy is
+$$
+Acc =\frac{5+2}{5+2+1+2}=0.7
+$$
+
+
+## Recall
+The recall of class $i$ is the ratio of correctly predicted class $i$ elements, among all elements, which truly belong to class $i$. The recall of class $P$ is:
+
+$$
+Rec_P =\frac{TP}{TP+FN}
+$$
+
+and for class $N$:
+
+$$
+Rec_N =\frac{TN}{TN+FP}
+$$
+
+In the example:
+
+$$
+Rec_1=\frac{2}{4} \quad \mbox{ and } \quad Rec_0=\frac{5}{6}
+$$
+
+
+## Precision 
+
+The precision of class $i$ is the ratio of true class $i$ elements, among all elements, which have been predicted to be class $i$. The precision of class $P$ is:
+
+
+$$
+Pre_P =\frac{TP}{TP+FP}
+$$
+
+and for class $N$:
+
+$$
+Pre_N =\frac{TN}{TN+FN}
+$$
+
+In the example:
+
+$$
+Pre_1=\frac{2}{3}  \quad \mbox{ and } \quad Pre_0=\frac{5}{7}
+$$
+
+## F1-Score
+
+The F1-score of a class $i$ is the harmonic mean of this class' precision and recall:
+
+$$
+F1_i = 2 \cdot \frac{Pre_i \cdot Rec_i}{Pre_i + Rec_i}
+$$
+
+In the example: 
+
+$$
+F1_1= 2 \cdot \frac{\frac{2}{4} \frac{2}{3}}{\frac{2}{4} + \frac{2}{3}}  \quad \mbox{ and } \quad F1_0= 2 \cdot \frac{\frac{5}{6} \frac{5}{7}}{\frac{5}{6} + \frac{5}{7}} 
+$$
+
+
---
+++ NLP/_build/jupyter_execute/06classification/FakeNewsClassification.py
@@ -0,0 +1,196 @@
+# Text Classification Application: Fake News detection
+* Author: Johannes Maucher
+* Last update: 24.11.2020
+
+In this notebook conventional Machine Learning algorithms are applied to learn a discriminator-model for distinguishing fake- and non-fake news.
+
+What you will learn:
+* Access text from .csv file
+* Preprocess text for classification
+* Calculate BoW matrix
+* Apply conventional machine learning algorithms for fake news detection
+* Evaluation of classifiers
+
+## Access Data
+In this notebook a [fake-news corpus from Kaggle](https://www.kaggle.com/c/fake-news/data) is applied for training and testing Machine Learning algorithms. Download the 3 files and save it in a directory. The path of this directory shall be assigned to the variable `path`in the following code-cell: 
+
+import pandas as pd
+pfad="/Users/johannes/DataSets/fake-news/"
+train = pd.read_csv(pfad+'train.csv',index_col=0)
+test = pd.read_csv(pfad+'test.csv',index_col=0)
+test_labels=pd.read_csv(pfad+'submit.csv',index_col=0)
+
+Data in dataframe `train` is applied for training. The dataframe `test`contains the texts for testing the model and the dataframe `test_labels` contains the true labels of the test-texts. 
+
+print("Number of texts in train-dataframe: \t",train.shape[0])
+print("Number of columns in train-dataframe: \t",train.shape[1])
+train.head()
+
+Append the test-dataframe with the labels, which are contained in dataframe `test_labels`.
+
+test["label"]=test_labels["label"]
+
+print("Number of texts in test-dataframe: \t",test.shape[0])
+print("Number of columns in test-dataframe: \t",test.shape[1])
+test.head()
+
+## Data Selection
+
+In the following code cells, first the number of missing-data fields is determined. Then the information in columns `author`, `title` and `text` are concatenated to a single string, which is saved in the column `total`. After this process, only columns `total` and `label` are required, all other columns can be removed in the `train`- and the `test`-dataframe. 
+
+train.isnull().sum(axis=0)
+
+test.isnull().sum(axis=0)
+
+train = train.fillna(' ')
+train['total'] = train['title'] + ' ' + train['author'] + ' ' + train['text']
+
+train = train[['total', 'label']]
+
+train.head()
+
+test = test.fillna(' ')
+test['total'] = test['title'] + ' ' + test['author'] + ' ' + test['text']
+test = test[['total', 'label']]
+
+## Preprocessing
+The input texts in column `total` shall be preprocessed as follows:
+* stopwords shall be removed
+* all characters, which are neither alpha-numeric nor whitespaces, shall be removed
+* all characters shall be represented in lower-case.
+* for all words, the lemma (base-form) shall be applied
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import re
+
+stop_words = stopwords.words('english')
+
+lemmatizer = WordNetLemmatizer()
+for index in train.index:
+    #filter_sentence = ''
+    sentence = train.loc[index,'total']
+    # Cleaning the sentence with regex
+    sentence = re.sub(r'[^\w\s]', '', sentence)
+    # Tokenization
+    words = nltk.word_tokenize(sentence)
+    # Stopwords removal
+    words = [lemmatizer.lemmatize(w).lower() for w in words if not w in stop_words]
+    filter_sentence = " ".join(words)
+    train.loc[index, 'total'] = filter_sentence
+
+First 5 cleaned texts in the training-dataframe:
+
+train.head()
+
+Clean data in the test-dataframe in the same way as done for the training-dataframe above:
+
+lemmatizer = WordNetLemmatizer()
+for index in test.index:
+    #filter_sentence = ''
+    sentence = test.loc[index,'total']
+    # Cleaning the sentence with regex
+    sentence = re.sub(r'[^\w\s]', '', sentence)
+    # Tokenization
+    words = nltk.word_tokenize(sentence)
+    # Stopwords removal
+    words = [lemmatizer.lemmatize(w).lower() for w in words if not w in stop_words]
+    filter_sentence = " ".join(words)
+    test.loc[index, 'total'] = filter_sentence
+
+First 5 cleaned texts in the test-dataframe:
+
+test.head()
+
+## Determine Bag-of-Word Matrix for Training- and Test-Data
+In the code-cells below two different types of Bag-of-Word matrices are calculated. The first type contains the **term-frequencies**, i.e. the entry in row $i$, column $j$ is the frequency of word $j$ in document $i$. In the second type, the matrix-entries are not the term-frequencies, but the tf-idf-values. 
+
+Note that for a given typ (term-frequency or tf-idf) a separate matrix must be calculated for training and testing. Since we always pretend, that only training-data is known in advance, the matrix-structure, i.e. the columns (= words) depends only on the training-data. This matrix structure is calculated in the row:
+
+```
+count_vectorizer.fit(X_train)
+```
+and
+```
+tfidf.fit(freq_term_matrix_train),
+```
+respectively. An important parameter of the `CountVectorizer`-class is `min_df`. The value, which is assigned to this parameter is the minimum frequency of a word, such that it is regarded in the BoW-matrix. Words, which appear less often are disregarded.
+
+The training data is then mapped to this structure by 
+```
+count_vectorizer.transform(X_train)
+```
+and
+```
+tfidf.transform(X_train),
+```
+respectively.
+
+For the test-data, however, no new matrix-structure is calculated. Instead the test-data is transformed to the structure of the matrix, defined by the training data.
+
+X_train = train['total'].values
+y_train = train['label'].values
+
+X_test = test['total'].values
+y_test = test['label'].values
+
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+
+Train BoW-models and transform training-data to BoW-matrix:
+
+count_vectorizer = CountVectorizer(min_df=4)
+count_vectorizer.fit(X_train)
+freq_term_matrix_train = count_vectorizer.transform(X_train)
+tfidf = TfidfTransformer(norm = "l2")
+tfidf.fit(freq_term_matrix_train)
+tf_idf_matrix_train = tfidf.transform(freq_term_matrix_train)
+
+freq_term_matrix_train.toarray().shape
+
+tf_idf_matrix_train.toarray().shape
+
+Transform test-data to BoW-matrix:
+
+freq_term_matrix_test = count_vectorizer.transform(X_test)
+tf_idf_matrix_test = tfidf.transform(freq_term_matrix_test)
+
+## Train a linear classificator
+Below a [Logistic Regression model](https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html#sklearn.linear_model.LogisticRegression) is trained. This is just a linear classifier with a sigmoid- or softmax- activity-function. 
+
+X_train=tf_idf_matrix_train
+X_test=tf_idf_matrix_test
+#X_train=freq_term_matrix_train
+#X_test=freq_term_matrix_test
+
+from sklearn.linear_model import LogisticRegression
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+
+## Evaluate trained model
+First, the trained model is applied to predict the class of the training-samples:
+
+y_pred_train = logreg.predict(X_train)
+
+y_pred_train
+
+from sklearn.metrics import confusion_matrix, classification_report
+
+confusion_matrix(y_train,y_pred_train)
+
+The model's prediction are compared with the true classes of the training-samples. The classification-report contains the common metrics for evaluating classifiers:
+
+print(classification_report(y_train,y_pred_train))
+
+The output of the classification report shows, that the model is well fitted to the training data, since it predicts training data with an accuracy of 98%.
+
+However, accuracy on the training-data, provides no information on the model's capability to classify new data. Therefore, below the model's prediction on the test-dataset is calculated:
+
+y_pred_test = logreg.predict(X_test)
+
+confusion_matrix(y_test,y_pred_test)
+
+print(classification_report(y_test,y_pred_test))
+
+The model's accuracy on the test-data is weak. The model is overfitted on the training-data. It seems that the distribution of test-data is significantly different from the distribution of training-data. This hypothesis can be verified by ignoring the data from `test.csv` and instead split data from `train.csv` into a train- and a test-partition. In this modified experiment performance on test-data is much better, because the texts within `train.csv` origin from the same distributions. 
